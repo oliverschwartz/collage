03c298862d4b46511607b110e6ff45033ee8edca
Oliver Schwartz <ollieschwartz99@gmail.com>
2021-09-04 18:16:13 +1000

    added data constructor for Dim

--- app/space.hs
+++ app/space.hs
@@ -4,8 +4,8 @@ module Space where
 
 type Width = Int
 type Height = Int
-type Dim = (Width, Height)
 type Coor = (Int, Int)
+data Dim = Dim (Width, Height) deriving (Eq, Show)
 
 data Frame = Frame {
     frameDim    :: Dim,
@@ -19,39 +19,35 @@ zeroCoor :: Coor
 zeroCoor = (0, 0)
 
 makeFrame :: Width -> Height -> Frame
-makeFrame w h = Frame (w, h) zeroCoor []
+makeFrame w h = Frame (Dim (w, h)) zeroCoor []
 
 -- Split an empty frame four ways.
 splitFrameFour :: Frame -> Int -> Int -> Either FrameError Frame
-splitFrameFour fr xSpl ySpl
-    | not . null $ (frameNest fr) = Left NonEmpty 
+splitFrameFour (Frame (Dim (w, h)) (fx, fy) nest) xSpl ySpl
+    | not . null $ nest = Left NonEmpty 
     | otherwise = 
-        let (w, h) = frameDim fr in 
         if (0 >= xSpl || xSpl >= w || 
             0 >= ySpl || ySpl >= h) then Left BadSplit
         else 
-            let (fx, fy) = frameCoor fr
-                bl = Frame (xSpl, ySpl)         (fx, fy)                []
-                br = Frame (w - xSpl, ySpl)     (fx + xSpl, fy)         []
-                tl = Frame (xSpl, h - ySpl)     (fx, fy + ySpl)         []
-                tr = Frame (w - xSpl, h - ySpl) (fx + xSpl, fy + ySpl)  []
-            in Right $ Frame (fx, fy) (w, h) [bl, br, tl, tr]
+            let bl = Frame (Dim (xSpl, ySpl))         (fx, fy)                []
+                br = Frame (Dim (w - xSpl, ySpl))     (fx + xSpl, fy)         []
+                tl = Frame (Dim (xSpl, h - ySpl))     (fx, fy + ySpl)         []
+                tr = Frame (Dim (w - xSpl, h - ySpl)) (fx + xSpl, fy + ySpl)  []
+            in Right $ Frame (Dim (w, h)) (fx, fy) [bl, br, tl, tr]
 
 -- Split an empty frame two ways. 
 splitFrameTwo :: Frame -> Int -> Split -> Either FrameError Frame
-splitFrameTwo fr s vOrH
-    | not . null $ (frameNest fr) = Left NonEmpty
+splitFrameTwo (Frame (Dim (w, h)) (fx, fy) nest) s vOrH
+    | not . null $ nest = Left NonEmpty
     | vOrH == V = 
         if (0 >= s || s >= w) then Left BadSplit
         else 
-            let l = Frame (s, h)     (fx, fy)       []
-                r = Frame (w - s, h) (fx + s, fy)   []
-            in Right $ Frame (fx, fy) (w, h) [l, r]
+            let l = Frame (Dim (s, h))     (fx, fy)       []
+                r = Frame (Dim (w - s, h)) (fx + s, fy)   []
+            in Right $ Frame (Dim (w, h)) (fx, fy) [l, r]
     | otherwise = -- vOrH == V
         if (0 >= s || s >= h) then Left BadSplit
         else 
-            let b = Frame (w, s)     (fx, fy)       []
-                t = Frame (w, h - s) (fx, fy + s)   []
-            in Right $ Frame (fx, fy) (w, h) [b, t]
-    where (w, h) = frameDim fr
-          (fx, fy) = frameCoor fr
+            let b = Frame (Dim (w, s))     (fx, fy)       []
+                t = Frame (Dim (w, h - s)) (fx, fy + s)   []
+            in Right $ Frame (Dim (w, h)) (fx, fy) [b, t]
