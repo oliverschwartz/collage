7a58d0335f4e1bdea214dcae30fb6a1a9cdaf4b0
Oliver Schwartz <ollieschwartz99@gmail.com>
2021-09-04 18:05:09 +1000

    dependencies and commenting

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,25 @@
 module Main where
 
+import System.Directory
+import Paths_collage
 import Space
 import ImageProcessor
 
+-- Only JPG images for now. 
+getImageNames :: IO [FilePath]
+getImageNames = do
+    filepath <- getDataFileName ""
+    let lastFour s = drop (length s - 4) s
+        isJpg s = elem (lastFour s) [".jpg", ".jpeg"]
+    contents <- getDirectoryContents filepath
+    return $ filter isJpg contents
+
 main :: IO ()
 main = do 
-    let (w, h) = (200, 400)
+    let (w, h) = (200, 600)
     getAndResizeImage "download.jpg" "saved.jpg" w h
-    putStrLn "done"
+    imageNames <- getImageNames 
+    let n = length imageNames
+    mapM_ putStrLn imageNames
     
 
